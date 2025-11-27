171555e9e04d3c01c00a2daa666100d41af4bbb7
farhadtheg <hotfrad@gmail.com>
2025-11-27 10:44:44 -0500

    Switch back to Google Analytics from Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,25 +26,22 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T545H5L8');`}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-66CFVJKXYD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-66CFVJKXYD');
+          `}
         </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-T545H5L8"
-            height="0" 
-            width="0" 
-            style={{display: 'none', visibility: 'hidden'}}
-          />
-        </noscript>
         {children}
       </body>
     </html>
